ffcefc158be1b8a35590ca27168f7efeb2631eec
danaik13 <shvechov8713@yandex.ru>
2018-03-09 11:04:54 +0400

    templetes and urls

---
+++ app/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from django.contrib import admin #Админка 
+
+
+from . import views
+
+
+urlpatterns = [
+   # url(r'^callback/TrsQw',      views.callback), 
+
+
+   
+]
--- olimp/settings.py
+++ olimp/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'olimp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],  #modify this line
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+LOGIN_REDIRECT_URL = '/'
+
+USE_TZ = True
+TIME_ZONE = 'Europe/Samara'
+
+DATE_INPUT_FORMATS = ('%d-%m-%Y')
